c0e487a6f97cf5b5849c9c368849436a50149698
rvt <github@rvt.dds.nl>
2026-01-09 10:43:48 +0100

    Handle preambles such that they are better compatible with other transceiver, mainly classic flarm

---
+++ src/pico/rp2350.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -e
+
+### ---- Build type selection ----
+BUILD_TYPE="${1:-Release}"
+
+case "$BUILD_TYPE" in
+    Debug|Release)
+        if [ $# -gt 0 ]; then
+            shift
+        fi
+        ;;
+    *)
+        echo "Invalid build type: $BUILD_TYPE"
+        echo "Use: Debug or Release"
+        exit 1
+        ;;
+esac
+
+BUILD_DIR="${BUILD_TYPE}_build_2350"
+UF2_NAME="GATAS_rp2350-arm-s.uf2"
+TARGET_DIR="/volumes/RP2350"
+
+CMAKE_ARGS="
+  -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+  -DPICO_PLATFORM=rp2350
+  -DPICO_BOARD=pico2_w
+  -DGATAS_UART_OVER_USB=1
+"
+
+echo "Configuring build directory: ${BUILD_DIR}"
+
+if command -v ninja >/dev/null 2>&1; then
+    GENERATOR="-G Ninja"
+    BUILD_CMD="ninja -C ${BUILD_DIR} $1"
+else
+    GENERATOR=""
+    BUILD_CMD="make -j $(getconf _NPROCESSORS_ONLN) -C ${BUILD_DIR} $1"
+    echo "Note: Ninja not found — using make (slower)."
+fi
+
+cmake -B "${BUILD_DIR}" ${GENERATOR} ${CMAKE_ARGS}
+eval "${BUILD_CMD}"
+
+# Copying stage
+UF2_PATH="${BUILD_DIR}/${UF2_NAME}"
+
+if [ -d "${TARGET_DIR}" ]; then
+    echo "###############################"
+    echo "## Copying to ${TARGET_DIR}"
+    echo "###############################"
+    cp "${UF2_PATH}" "${TARGET_DIR}"
+else
+    echo "########################"
+    echo "## Please mount the PI #"
+    echo "########################"
+fi
