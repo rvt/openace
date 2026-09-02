80dce8b06c8861d4fc3c6b17d97e92460695e5dd
R. van Twisk <github@rvt.dds.nl>
2026-09-02 22:23:06 +0200

    Better clearing ordering of IRQ Status

--- src/lib/gps/ace/src/StaticGPS.cpp
+++ src/lib/gps/ace/src/StaticGPS.cpp
@@ -233,13 +233,13 @@ void StaticGPS::publishSentences()
     GATAS::NMEAString nmeaString;
 
     // $GPGLL
-    etl::string<3> AV;
+    const char *AV;
     AV = (valid ? ",A" : ",V");
     {
         etl::string_stream nmeaStream(nmeaString);
         nmeaStream << "$GPGLL," << latitudeCoordinate.text << "," << etl::string_view(&latitudeCoordinate.hemisphere, 1)
                    << "," << longitudeCoordinate.text << "," << etl::string_view(&longitudeCoordinate.hemisphere, 1)
-                   << ',' << timeText << AV;
+                   << "," << timeText << AV;
     }
     CoreUtils::addChecksumToNMEA(nmeaString, false);
     processNewSentence({nmeaString.data(), nmeaString.size()});
